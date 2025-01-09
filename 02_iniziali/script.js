a883e99b7f55d92cb2574d1bb1484550e8e49d9a
Alex Mihov <alexmihov.as@gmail.com>
2025-01-09 15:19:18 +0100

    feat 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,11 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function inizialiLista(nome) {
-    
+function inizialiNomiLista(names) {
+    // utilizzo map per creare un nuovo array applicando la funzione charAt(0) che restituisce il primo elemento di ogni nome 
+    return names.map(nome => nome.charAt(0));
 }
+
+// creo una costante che abbia come valore la funzione inizialiNomiLista(names)
+const iniziali = inizialiNomiLista(names);
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali);
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
